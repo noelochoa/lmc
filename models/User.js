9de49ea7b010692d917ab65e3ede6a86e3eeb790
noelochoa <35244256+noelochoa@users.noreply.github.com>
2020-09-23 22:40:21 +0900

    ssr

--- models/User.js
+++ models/User.js
@@ -65,7 +65,7 @@ userSchema.methods.generateAuthToken = async function () {
 	// Generate an auth token for the user
 	const user = this
 	const token = jwt.sign({ _id: user._id }, process.env.JWT_KEY, {
-		expiresIn: '1m',
+		expiresIn: '15m',
 	})
 	user.tokens = user.tokens.concat({ token })
 	await user.save()
