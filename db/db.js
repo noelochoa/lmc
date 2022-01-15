83c1e6286fbe7b58d55a616351ae8c6b074b273e
noelochoa <35244256+noelochoa@users.noreply.github.com>
2022-01-15 20:12:55 +0900

    Fix dependencies

--- db/db.js
+++ db/db.js
@@ -3,8 +3,7 @@ const mongoose = require('mongoose')
 mongoose
 	.connect(process.env.MONGODB_URL, {
 		useNewUrlParser: true,
-		useUnifiedTopology: true,
-		useCreateIndex: false
+		useUnifiedTopology: true
 	})
 	.then((res) => {
 		console.log('MongoDB connected!')
